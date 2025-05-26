3cc79c6231801dffab4e5686201290c3554f723c
Tom Fan <119341778+TomFanHM@users.noreply.github.com>
2025-05-26 23:02:02 +0800

    feat: enhance layout

---
+++ web/src/config/site.ts
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  name: "Misaka Network",
+  description: "",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "",
+    github: "",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
